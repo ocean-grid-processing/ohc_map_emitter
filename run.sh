eb6b7a765ac9ef614dfa13c8142c3b04e2e094df
Katie Mills <you@example.com>
2026-08-11 12:01:25 -0600

    run config

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 for level in 15_20 15_300 300_700 700_1000 700_1850 1800_1850
 do
-    sbatch combine.slurm ${level}
+    dir=/scratch/alpine/wimi7695/ohc_prod/potential_temperature/OP20260507
+    sbatch combine.slurm ${dir}
 done
