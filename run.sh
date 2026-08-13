42b7abab1f5a358ad8d8df5b9905f24334e59113
Katie Mills <you@example.com>
2026-08-12 21:37:09 -0600

    run config rc for prod_260810

--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
-for level in 15_20 15_300 300_700 700_1000 700_1850 1800_1850
-do
-    dir=/scratch/alpine/wimi7695/ohc_prod/potential_temperature/OP20260507
-    sbatch combine.slurm ${dir}
-done
+sbatch combine.slurm prod_260810
